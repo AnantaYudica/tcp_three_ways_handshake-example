cb695da691c135864d43609b5166050285a03560
Ananta Yudica <nta040689@gmail.com>
2019-07-18 08:11:26 +0700

    Added dependency with bytes/Pointer.h Added member variable 'm_index' Updated operator=(Byte&&) from swap variable to create new instance variable

--- include/Byte.h
+++ include/Byte.h
@@ -6,14 +6,19 @@
 #include <cstdint>
 #include <memory>
 
+#include "bytes/Pointer.h"
+
 class Byte
 {
 private:
     std::shared_ptr<std::uint8_t> m_ptr;
+    std::size_t m_index;
 public:
     inline Byte();
     inline Byte(const std::uint8_t & b);
     inline Byte(const std::shared_ptr<std::uint8_t> & b);
+    inline Byte(const std::shared_ptr<std::uint8_t> & b, 
+        const std::size_t & i);
     inline ~Byte();
 public:
     inline Byte(const Byte & cpy);
@@ -116,72 +121,85 @@ public:
 };
 
 inline Byte::Byte() :
-    m_ptr(new std::uint8_t(0))
+    m_ptr(new std::uint8_t(0)),
+    m_index(0)
 {}
 
 inline Byte::Byte(const std::uint8_t & b) :
-    m_ptr(new std::uint8_t(b))
+    m_ptr(new std::uint8_t(b)),
+    m_index(0)
 {}
 
 inline Byte::Byte(const std::shared_ptr<std::uint8_t> & b) :
-    m_ptr(b)
+    m_ptr(b),
+    m_index(0)
+{}
+
+inline Byte::Byte(const std::shared_ptr<std::uint8_t> & b, 
+    const std::size_t & i) :
+        m_ptr(b),
+        m_index(i)
 {}
 
 inline Byte::~Byte()
 {
     m_ptr = nullptr;
+    m_index = 0;
 }
 
 inline Byte::Byte(const Byte & cpy) :
-    m_ptr(new std::uint8_t(*(cpy.m_ptr)))
+    m_ptr(new std::uint8_t(*(cpy.m_ptr))),
+    m_index(0)
 {}
 
 inline Byte::Byte(Byte && mov) :
-    m_ptr(mov.m_ptr)
+    m_ptr(mov.m_ptr),
+    m_index(mov.m_index)
 {
     mov.m_ptr = std::make_shared<std::uint8_t>(0);
+    mov.m_index = 0;
 }
 
 inline Byte & Byte::operator=(Byte && b)
 {
-    auto cpy = m_ptr;
     m_ptr = b.m_ptr;
-    b.m_ptr = cpy;
-    *b.m_ptr = 0;
+    m_index = b.m_index;
+    b.m_ptr = std::make_shared<std::uint8_t>(0);
+    b.m_index = 0;
     return *this;
 }
 
 inline Byte & Byte::operator=(const Byte & b)
 {
-    return (*this = *(b.m_ptr));
+    return (*this = *bytes::Pointer(b.m_ptr, m_index));
 }
 
 inline Byte & Byte::operator=(const std::uint8_t & b)
 {
-    *m_ptr = b;
+    *bytes::Pointer(m_ptr, m_index) = b;
     return *this;
 }
 
 inline Byte::operator std::uint8_t() const
 {
-    return *m_ptr;
+    return *bytes::Pointer(m_ptr, m_index);
 }
 
 inline Byte Byte::operator~() const
 {
     Byte res(*this);
-    *res.m_ptr = ~*(res.m_ptr);
+    res = ~*bytes::Pointer(res.m_ptr, res.m_index);
     return res;
 }
 
 inline Byte & Byte::operator|=(const Byte & b)
 {
-    return (*this |= *(b.m_ptr));
+    return (*this |= *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline Byte & Byte::operator|=(const std::uint8_t & b)
 {
-    *m_ptr |= b;
+    *bytes::Pointer(m_ptr, m_index) |= b;
     return *this;
 }
 
@@ -201,12 +219,12 @@ inline Byte Byte::operator|(const std::uint8_t & b) const
 
 inline Byte & Byte::operator&=(const Byte & b)
 {
-    return (*this &= *(b.m_ptr));
+    return (*this &= *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline Byte & Byte::operator&=(const std::uint8_t & b)
 {
-    *m_ptr &= b;
+    *bytes::Pointer(m_ptr, m_index) &= b;
     return *this;
 }
 
@@ -226,12 +244,12 @@ inline Byte Byte::operator&(const std::uint8_t & b) const
 
 inline Byte & Byte::operator^=(const Byte & b)
 {
-    return (*this ^= *(b.m_ptr));
+    return (*this ^= *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline Byte & Byte::operator^=(const std::uint8_t & b)
 {
-    *m_ptr ^= b;
+    *bytes::Pointer(m_ptr, m_index) ^= b;
     return *this;
 }
 
@@ -251,7 +269,7 @@ inline Byte Byte::operator^(const std::uint8_t & b) const
 
 inline Byte & Byte::operator>>=(const std::uint8_t & b)
 {
-    *m_ptr >>= b;
+    *bytes::Pointer(m_ptr, m_index) >>= b;
     return *this;
 }
 
@@ -264,7 +282,7 @@ inline Byte Byte::operator>>(const std::uint8_t & b) const
 
 inline Byte & Byte::operator<<=(const std::uint8_t & b)
 {
-    *m_ptr <<= b;
+    *bytes::Pointer(m_ptr, m_index) <<= b;
     return *this;
 }
 
@@ -301,12 +319,12 @@ inline Byte Byte::operator--(int) const
 
 inline Byte & Byte::operator+=(const Byte & b)
 {
-    return (*this += *(b.m_ptr));
+    return (*this += *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline Byte & Byte::operator+=(const std::uint8_t & b)
 {
-    *m_ptr += b;
+    *bytes::Pointer(m_ptr, m_index) += b;
     return *this;
 }
 
@@ -326,12 +344,12 @@ inline Byte Byte::operator+(const std::uint8_t & b) const
 
 inline Byte & Byte::operator-=(const Byte & b)
 {
-    return (*this -= *(b.m_ptr));
+    return (*this -= *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline Byte & Byte::operator-=(const std::uint8_t & b)
 {
-    *m_ptr -= b;
+    *bytes::Pointer(m_ptr, m_index) -= b;
     return *this;
 }
 
@@ -351,12 +369,12 @@ inline Byte Byte::operator-(const std::uint8_t & b) const
 
 inline Byte & Byte::operator*=(const Byte & b)
 {
-    return (*this *= *(b.m_ptr));
+    return (*this *= *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline Byte & Byte::operator*=(const std::uint8_t & b)
 {
-    *m_ptr *= b;
+    *bytes::Pointer(m_ptr, m_index) *= b;
     return *this;
 }
 
@@ -376,12 +394,12 @@ inline Byte Byte::operator*(const std::uint8_t & b) const
 
 inline Byte & Byte::operator/=(const Byte & b)
 {
-    return (*this /= *(b.m_ptr));
+    return (*this /= *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline Byte & Byte::operator/=(const std::uint8_t & b)
 {
-    *m_ptr /= b;
+    *bytes::Pointer(m_ptr, m_index) /= b;
     return *this;
 }
 
@@ -401,12 +419,12 @@ inline Byte Byte::operator/(const std::uint8_t & b) const
 
 inline Byte & Byte::operator%=(const Byte & b)
 {
-    return (*this %= *(b.m_ptr));
+    return (*this %= *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline Byte & Byte::operator%=(const std::uint8_t & b)
 {
-    *m_ptr %= b;
+    *bytes::Pointer(m_ptr, m_index) %= b;
     return *this;
 }
 
@@ -426,22 +444,22 @@ inline Byte Byte::operator%(const std::uint8_t & b) const
 
 inline Bit Byte::operator[](const std::uint8_t & i)
 {
-    return {m_ptr, i};
+    return {m_ptr, i, m_index};
 }
 
 inline const Bit Byte::operator[](const std::uint8_t & i) const
 {
-    return {m_ptr, i};
+    return {const_cast<std::shared_ptr<std::uint8_t> &>(m_ptr), i, m_index};
 }
 
 inline bool Byte::operator==(const Byte & b) const
 {
-    return (*this == *(b.m_ptr));
+    return (*this == *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline bool Byte::operator==(const std::uint8_t & b) const
 {
-    return *m_ptr == b;
+    return *bytes::Pointer(m_ptr, m_index) == b;
 }
 
 inline bool Byte::operator!=(const Byte & b) const
@@ -456,42 +474,42 @@ inline bool Byte::operator!=(const std::uint8_t & b) const
 
 inline bool Byte::operator<(const Byte & b) const
 {
-    return (*this < *(b.m_ptr));
+    return (*this < *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline bool Byte::operator<(const std::uint8_t & b) const
 {
-    return *m_ptr < b;
+    return *bytes::Pointer(m_ptr, m_index) < b;
 }
 
 inline bool Byte::operator<=(const Byte & b) const
 {
-    return (*this <= *(b.m_ptr));
+    return (*this <= *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline bool Byte::operator<=(const std::uint8_t & b) const
 {
-    return *m_ptr <= b;
+    return *bytes::Pointer(m_ptr, m_index) <= b;
 }
 
 inline bool Byte::operator>(const Byte & b) const
 {
-    return (*this > *(b.m_ptr));
+    return (*this > *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline bool Byte::operator>(const std::uint8_t & b) const
 {
-    return *m_ptr > b;
+    return *bytes::Pointer(m_ptr, m_index) > b;
 }
 
 inline bool Byte::operator>=(const Byte & b) const
 {
-    return (*this >= *(b.m_ptr));
+    return (*this >= *bytes::Pointer(b.m_ptr, b.m_index));
 }
 
 inline bool Byte::operator>=(const std::uint8_t & b) const
 {
-    return *m_ptr >= b;
+    return *bytes::Pointer(m_ptr, m_index) >= b;
 }
 
 inline Byte operator|(const std::uint8_t & a, const Byte & b)
