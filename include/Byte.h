563b905a48c0d73b11be95c58014e4acb7a35876
Ananta Yudica <nta040689@gmail.com>
2019-07-21 15:54:25 +0700

    Added dependency with 'bytes/ptr/Segment.h' Updated type of m_ptr member variable Added m_segment member variable Remove m_index member variable Updated constructor Added IsSamePointer member function Updated operator-- and operator++ member functions with int as parameter Removed operator== and operator!= member functions with share pointer of std::uint8_t as parameter

--- include/Byte.h
+++ include/Byte.h
@@ -7,21 +7,22 @@
 #include <memory>
 
 #include "bytes/Pointer.h"
+#include "bytes/ptr/Segment.h"
 
 class Byte
 {
 private:
     static inline void Default(Byte & b);
-    static inline void Validation(Byte & b);
+    static inline bool Validation(Byte & b);
 private:
-    std::shared_ptr<std::uint8_t> m_ptr;
-    std::size_t m_index;
+    std::shared_ptr<bytes::Pointer> m_ptr;
+    std::shared_ptr<bytes::ptr::Segment> m_segment;
 public:
     inline Byte();
     inline Byte(const std::uint8_t & b);
-    inline Byte(const std::shared_ptr<std::uint8_t> & b);
-    inline Byte(const std::shared_ptr<std::uint8_t> & b, 
-        const std::size_t & i);
+    inline Byte(const std::shared_ptr<bytes::Pointer> & b);
+    inline Byte(const std::shared_ptr<bytes::Pointer> & b, 
+        const std::shared_ptr<bytes::ptr::Segment> & seg);
     inline ~Byte();
 public:
     inline Byte(const Byte & cpy);
@@ -30,6 +31,9 @@ public:
     inline Byte & operator=(Byte && b);
     inline Byte & operator=(const Byte & b);
     inline Byte & operator=(const std::uint8_t & b);
+public:
+    inline bool IsSamePointer(const Byte & b) const;
+    inline bool IsSamePointer(const std::shared_ptr<bytes::Pointer> & b) const;
 public:
     inline operator std::uint8_t() const;
 public:
@@ -66,10 +70,10 @@ public:
     inline Byte operator-() const = delete;
 public:
     inline Byte & operator++();
-    inline Byte operator++(int) const;
+    inline Byte operator++(int);
 public:
     inline Byte & operator--();
-    inline Byte operator--(int) const;
+    inline Byte operator--(int);
 public:
     inline Byte & operator+=(const Byte & b);
     inline Byte & operator+=(const std::uint8_t & b);
@@ -106,11 +110,9 @@ public:
 public:
     inline bool operator==(const Byte & b) const;
     inline bool operator==(const std::uint8_t & b) const;
-    inline bool operator==(const std::shared_ptr<std::uint8_t> & b) const;
 public:
     inline bool operator!=(const Byte & b) const;
     inline bool operator!=(const std::uint8_t & b) const;
-    inline bool operator!=(const std::shared_ptr<std::uint8_t> & b) const;
 public:
     inline bool operator<(const Byte & b) const;
     inline bool operator<(const std::uint8_t & b) const;
@@ -127,54 +129,71 @@ public:
 
 inline void Byte::Default(Byte & b)
 {
-    b.m_ptr = std::make_shared<std::uint8_t>(0);
-    b.m_index = 0;
+    b.m_ptr = std::make_shared<bytes::Pointer>(1);
+    b.m_ptr->At(0) = 0;
+    b.m_segment = b.m_ptr->Share(0, 1);
 }
 
-inline void Byte::Validation(Byte & b)
+inline bool Byte::Validation(Byte & b)
 {
-    if (!b.m_ptr) Default(b);
+    if (!b.m_ptr && !*(b.m_ptr)) 
+    {
+        Default(b);
+        return false;
+    }
+    return true;
 }
 
 inline Byte::Byte() :
-    m_ptr(new std::uint8_t(0)),
-    m_index(0)
+    m_ptr(new bytes::Pointer(1)),
+    m_segment(m_ptr->Share(0, 1))
 {}
 
 inline Byte::Byte(const std::uint8_t & b) :
-    m_ptr(new std::uint8_t(b)),
-    m_index(0)
-{}
-
-inline Byte::Byte(const std::shared_ptr<std::uint8_t> & b) :
-    m_ptr(b),
-    m_index(0)
+    m_ptr(new bytes::Pointer(1)),
+    m_segment(m_ptr->Share(0, 1))
 {
-    Validation(*this);
+    m_ptr->At(0) = b;
 }
 
-inline Byte::Byte(const std::shared_ptr<std::uint8_t> & b, 
-    const std::size_t & i) :
-        m_ptr(b),
-        m_index(i)
+inline Byte::Byte(const std::shared_ptr<bytes::Pointer> & b) :
+    m_ptr(b)
 {
-    Validation(*this);
+    if (Validation(*this)) m_segment = m_ptr->Share(0, 1);
+}
+
+inline Byte::Byte(const std::shared_ptr<bytes::Pointer> & b, 
+    const std::shared_ptr<bytes::ptr::Segment> & seg) :
+        m_ptr(b)
+{
+    if (Validation(*this))
+    {
+        if ((seg && *seg) && m_ptr->Has(seg))
+            m_segment = seg;
+        else if (seg && *seg)
+            m_segment = m_ptr->Share(seg->Begin(), seg->End());
+        else
+            m_segment = m_ptr->Share(0, 1);
+    }
 }
 
 inline Byte::~Byte()
 {
+    m_ptr->Reset(m_segment);
+    m_segment = nullptr;
     m_ptr = nullptr;
-    m_index = 0;
 }
 
 inline Byte::Byte(const Byte & cpy) :
-    m_ptr(new std::uint8_t(*(cpy.m_ptr))),
-    m_index(0)
-{}
+    m_ptr(new bytes::Pointer(1))
+{
+    m_segment = m_ptr->Share(0, 1);
+    (*this) = cpy;
+}
 
 inline Byte::Byte(Byte && mov) :
     m_ptr(mov.m_ptr),
-    m_index(mov.m_index)
+    m_segment(mov.m_segment)
 {
     Default(mov);
 }
@@ -182,42 +201,53 @@ inline Byte::Byte(Byte && mov) :
 inline Byte & Byte::operator=(Byte && b)
 {
     m_ptr = b.m_ptr;
-    m_index = b.m_index;
+    m_segment = b.m_segment;
     Default(b);
     return *this;
 }
 
 inline Byte & Byte::operator=(const Byte & b)
 {
-    return (*this = *bytes::Pointer(b.m_ptr, m_index));
+    return (*this = b.m_ptr->At(m_segment, 0));
 }
 
 inline Byte & Byte::operator=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) = b;
+    m_ptr->At(m_segment, 0) = b;
     return *this;
+}
+
+inline bool Byte::IsSamePointer(const Byte & b) const
+{
+    return m_ptr == b.m_ptr;
+}
+
+inline bool 
+    Byte::IsSamePointer(const std::shared_ptr<bytes::Pointer> & b) const
+{
+    return m_ptr == b;
 }
 
 inline Byte::operator std::uint8_t() const
 {
-    return *bytes::Pointer(m_ptr, m_index);
+    return m_ptr->At(m_segment, 0);
 }
 
 inline Byte Byte::operator~() const
 {
     Byte res(*this);
-    res = ~*bytes::Pointer(res.m_ptr, res.m_index);
+    res = ~static_cast<std::uint8_t>(res);
     return res;
 }
 
 inline Byte & Byte::operator|=(const Byte & b)
 {
-    return (*this |= *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this |= static_cast<std::uint8_t>(b));
 }
 
 inline Byte & Byte::operator|=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) |= b;
+    m_ptr->At(m_segment, 0) |= b;
     return *this;
 }
 
@@ -237,12 +267,12 @@ inline Byte Byte::operator|(const std::uint8_t & b) const
 
 inline Byte & Byte::operator&=(const Byte & b)
 {
-    return (*this &= *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this &= static_cast<std::uint8_t>(b));
 }
 
 inline Byte & Byte::operator&=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) &= b;
+    m_ptr->At(m_segment, 0) &= b;
     return *this;
 }
 
@@ -262,12 +292,12 @@ inline Byte Byte::operator&(const std::uint8_t & b) const
 
 inline Byte & Byte::operator^=(const Byte & b)
 {
-    return (*this ^= *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this ^= static_cast<std::uint8_t>(b));
 }
 
 inline Byte & Byte::operator^=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) ^= b;
+    m_ptr->At(m_segment, 0) ^= b;
     return *this;
 }
 
@@ -287,7 +317,7 @@ inline Byte Byte::operator^(const std::uint8_t & b) const
 
 inline Byte & Byte::operator>>=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) >>= b;
+    m_ptr->At(m_segment, 0) >>= b;
     return *this;
 }
 
@@ -300,7 +330,7 @@ inline Byte Byte::operator>>(const std::uint8_t & b) const
 
 inline Byte & Byte::operator<<=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) <<= b;
+    m_ptr->At(m_segment, 0) <<= b;
     return *this;
 }
 
@@ -316,10 +346,10 @@ inline Byte & Byte::operator++()
     return (*this += 1);
 }
 
-inline Byte Byte::operator++(int) const
+inline Byte Byte::operator++(int)
 {
     Byte res(*this);
-    ++res;
+    ++(*this);
     return res;
 }
 
@@ -328,21 +358,21 @@ inline Byte & Byte::operator--()
     return (*this -= 1);
 }
 
-inline Byte Byte::operator--(int) const
+inline Byte Byte::operator--(int)
 {
     Byte res(*this);
-    --res;
+    --(*this);
     return res;
 }
 
 inline Byte & Byte::operator+=(const Byte & b)
 {
-    return (*this += *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this += static_cast<std::uint8_t>(b));
 }
 
 inline Byte & Byte::operator+=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) += b;
+    m_ptr->At(m_segment, 0) += b;
     return *this;
 }
 
@@ -362,12 +392,12 @@ inline Byte Byte::operator+(const std::uint8_t & b) const
 
 inline Byte & Byte::operator-=(const Byte & b)
 {
-    return (*this -= *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this -= static_cast<std::uint8_t>(b));
 }
 
 inline Byte & Byte::operator-=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) -= b;
+    m_ptr->At(m_segment, 0) -= b;
     return *this;
 }
 
@@ -387,12 +417,12 @@ inline Byte Byte::operator-(const std::uint8_t & b) const
 
 inline Byte & Byte::operator*=(const Byte & b)
 {
-    return (*this *= *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this *= static_cast<std::uint8_t>(b));
 }
 
 inline Byte & Byte::operator*=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) *= b;
+    m_ptr->At(m_segment, 0) *= b;
     return *this;
 }
 
@@ -412,12 +442,12 @@ inline Byte Byte::operator*(const std::uint8_t & b) const
 
 inline Byte & Byte::operator/=(const Byte & b)
 {
-    return (*this /= *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this /= static_cast<std::uint8_t>(b));
 }
 
 inline Byte & Byte::operator/=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) /= b;
+    m_ptr->At(m_segment, 0) /= b;
     return *this;
 }
 
@@ -437,12 +467,12 @@ inline Byte Byte::operator/(const std::uint8_t & b) const
 
 inline Byte & Byte::operator%=(const Byte & b)
 {
-    return (*this %= *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this %= static_cast<std::uint8_t>(b));
 }
 
 inline Byte & Byte::operator%=(const std::uint8_t & b)
 {
-    *bytes::Pointer(m_ptr, m_index) %= b;
+    m_ptr->At(m_segment, 0) %= b;
     return *this;
 }
 
@@ -462,27 +492,23 @@ inline Byte Byte::operator%(const std::uint8_t & b) const
 
 inline Bit Byte::operator[](const std::uint8_t & i)
 {
-    return {m_ptr, i, m_index};
+    return {m_ptr, i, m_ptr->Share(m_segment)};
 }
 
 inline const Bit Byte::operator[](const std::uint8_t & i) const
 {
-    return {const_cast<std::shared_ptr<std::uint8_t> &>(m_ptr), i, m_index};
+    Bit res{m_ptr->At(m_segment, 0), i};
+    return res;
 }
 
 inline bool Byte::operator==(const Byte & b) const
 {
-    return (*this == *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this == static_cast<std::uint8_t>(b));
 }
 
 inline bool Byte::operator==(const std::uint8_t & b) const
 {
-    return *bytes::Pointer(m_ptr, m_index) == b;
-}
-
-inline bool Byte::operator==(const std::shared_ptr<std::uint8_t> & b) const
-{
-    return m_ptr == b;
+    return m_ptr->At(m_segment, 0) == b;
 }
 
 inline bool Byte::operator!=(const Byte & b) const
@@ -495,49 +521,44 @@ inline bool Byte::operator!=(const std::uint8_t & b) const
     return !(*this == b);
 }
 
-inline bool Byte::operator!=(const std::shared_ptr<std::uint8_t> & b) const
-{
-    return !(*this == b);
-}
-
 inline bool Byte::operator<(const Byte & b) const
 {
-    return (*this < *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this < static_cast<std::uint8_t>(b));
 }
 
 inline bool Byte::operator<(const std::uint8_t & b) const
 {
-    return *bytes::Pointer(m_ptr, m_index) < b;
+    return m_ptr->At(m_segment, 0) < b;
 }
 
 inline bool Byte::operator<=(const Byte & b) const
 {
-    return (*this <= *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this <= static_cast<std::uint8_t>(b));
 }
 
 inline bool Byte::operator<=(const std::uint8_t & b) const
 {
-    return *bytes::Pointer(m_ptr, m_index) <= b;
+    return m_ptr->At(m_segment, 0) <= b;
 }
 
 inline bool Byte::operator>(const Byte & b) const
 {
-    return (*this > *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this > static_cast<std::uint8_t>(b));
 }
 
 inline bool Byte::operator>(const std::uint8_t & b) const
 {
-    return *bytes::Pointer(m_ptr, m_index) > b;
+    return m_ptr->At(m_segment, 0) > b;
 }
 
 inline bool Byte::operator>=(const Byte & b) const
 {
-    return (*this >= *bytes::Pointer(b.m_ptr, b.m_index));
+    return (*this >= static_cast<std::uint8_t>(b));
 }
 
 inline bool Byte::operator>=(const std::uint8_t & b) const
 {
-    return *bytes::Pointer(m_ptr, m_index) >= b;
+    return m_ptr->At(m_segment, 0) >= b;
 }
 
 inline Byte operator|(const std::uint8_t & a, const Byte & b)
@@ -585,20 +606,7 @@ inline bool operator==(const std::uint8_t & a, const Byte & b)
     return b == a;
 }
 
-inline bool operator==(const std::shared_ptr<std::uint8_t> & a,
-    const Byte & b)
-{
-    return b == a;
-}
-
-
 inline bool operator!=(const std::uint8_t & a, const Byte & b)
-{
-    return b != a;
-}
-
-inline bool operator!=(const std::shared_ptr<std::uint8_t> & a,
-    const Byte & b)
 {
     return b != a;
 }
