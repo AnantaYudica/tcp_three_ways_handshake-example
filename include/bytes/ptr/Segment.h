1c657cca184a50cae348c4938e770e13a5e5b93c
Ananta Yudica <nta040689@gmail.com>
2019-07-22 09:25:11 +0700

    Added dependencies with memory and Object Added member variable 'm_object' with shared pointer of Object Updated constructor with added shared pointer of Object as parameter Added comparison variable object with self object Removed member functions 'Begin' and 'End' with type std::size_t as parameter Removed member function 'Resize' Removed member functions 'operator==' and 'operator!=' with pair type of std::size_t as parameter Added member functions 'At', 'Get' and 'operator[]'

--- include/bytes/ptr/Segment.h
+++ include/bytes/ptr/Segment.h
@@ -3,6 +3,9 @@
 
 #include <cstdint>
 #include <utility>
+#include <memory>
+
+#include "Object.h"
 
 namespace bytes
 {
@@ -14,9 +17,11 @@ class Segment
 private:
     std::size_t m_begin;
     std::size_t m_end;
+    std::shared_ptr<bytes::ptr::Object> m_object;
 public:
     inline Segment();
-    inline Segment(const std::size_t & bg, const std::size_t & ed);
+    inline Segment(const std::size_t & bg, const std::size_t & ed,
+        const std::shared_ptr<bytes::ptr::Object> & o);
     inline ~Segment();
 public:
     inline Segment(const Segment & cpy);
@@ -25,62 +30,73 @@ public:
     inline Segment & operator=(const Segment & cpy);
     inline Segment & operator=(Segment && mov);
 public:
+    inline bool IsSameObject(const Segment & seg) const;
+public:
     inline std::size_t Begin() const;
-    inline void Begin(const std::size_t & bg);
 public:
     inline std::size_t End() const;
-    inline void End(const std::size_t & ed);
 public:
     inline std::size_t Size() const;
 public:
-    inline void Resize(const std::size_t & i, const std::size_t & s,
-        const std::size_t & ns);
+    inline std::uint8_t & At(const std::size_t & i);
+    inline const std::uint8_t & At(const std::size_t & i) const;
+public:
+    inline std::uint8_t * Get();
+    inline const std::uint8_t * Get() const;
 public:
     inline operator bool() const;
 public:
+    inline std::uint8_t & operator[](const std::size_t & i);
+    inline const std::uint8_t & operator[](const std::size_t & i) const;
+public:
     inline bool operator==(const Segment & b) const;
-    inline bool operator==(const std::pair<std::size_t, 
-        std::size_t> & b) const;
 public:
     inline bool operator!=(const Segment & b) const;
-    inline bool operator!=(const std::pair<std::size_t, 
-        std::size_t> & b) const;
 };
 
 inline Segment::Segment() :
     m_begin(0),
-    m_end(0)
+    m_end(0),
+    m_object(nullptr)
 {}
 
-inline Segment::Segment(const std::size_t & bg, const std::size_t & ed) :
-    m_begin(bg),
-    m_end(ed)
+inline Segment::Segment(const std::size_t & bg, const std::size_t & ed,
+    const std::shared_ptr<bytes::ptr::Object> & o) :
+        m_begin(bg),
+        m_end(ed),
+        m_object(o)
 {}
 
 inline Segment::~Segment()
 {
     m_begin = 0;
     m_end = 0;
+    m_object = nullptr;
 }
 
 inline Segment::Segment(const Segment & cpy) :
     m_begin(cpy.m_begin),
-    m_end(cpy.m_end)
+    m_end(cpy.m_end),
+    m_object(std::make_shared<bytes::ptr::Object>(cpy.m_object, 
+        m_begin, m_end))
 {}
 
 inline Segment::Segment(Segment && mov) :
     m_begin(mov.m_begin),
-    m_end(mov.m_end)
-
+    m_end(mov.m_end),
+    m_object(mov.m_object)
 {
     mov.m_begin = 0;
     mov.m_end = 0;
+    mov.m_object = nullptr;
 }
 
 inline Segment & Segment::operator=(const Segment & cpy)
 {
     m_begin = cpy.m_begin;
     m_end = cpy.m_end;
+    m_object = std::make_shared<bytes::ptr::Object>(cpy.m_object,
+        m_begin, m_end);
     return *this;
 }
 
@@ -88,9 +104,16 @@ inline Segment & Segment::operator=(Segment && mov)
 {
     m_begin = mov.m_begin;
     m_end = mov.m_end;
+    m_object = mov.m_object;
     mov.m_begin = 0;
     mov.m_end = 0;
+    mov.m_object = nullptr;
     return *this;
+}
+
+inline bool Segment::IsSameObject(const Segment & seg) const
+{
+    return seg.m_object == m_object;
 }
 
 inline std::size_t Segment::Begin() const
@@ -98,19 +121,9 @@ inline std::size_t Segment::Begin() const
     return m_begin;
 }
 
-inline void Segment::Begin(const std::size_t & bg)
-{
-    m_begin = bg;
-}
-
 inline std::size_t Segment::End() const
 {
     return m_end;
-}
-
-inline void Segment::End(const std::size_t & ed)
-{
-    m_end = ed;
 }
 
 inline std::size_t Segment::Size() const
@@ -118,46 +131,52 @@ inline std::size_t Segment::Size() const
     return m_end - m_begin;
 }
 
-inline void Segment::Resize(const std::size_t & i, const std::size_t & s,
-    const std::size_t & ns)
+inline std::uint8_t & Segment::At(const std::size_t & i)
 {
-    if (s == ns) return;
-    const bool isinc = s < ns;
-    const std::size_t d = (isinc ? ns - s : s - ns);
-    const bool isbg  = Begin() > (i + d), ised = End() > (i + d);
-    if (isbg && isinc)
-        Begin(Begin() + d);
-    else if(isbg && !isinc) 
-        Begin(Begin() - d);
-    if (ised && isinc)
-        End(End() + d);
-    else if (ised && !isinc)
-        End(End() - d);
+    if (!m_object) return *bytes::ptr::Object::Null();
+    return m_object->At(m_begin + i);
+}
+
+inline const std::uint8_t & Segment::At(const std::size_t & i) const
+{
+    if (!m_object) return *bytes::ptr::Object::Null();
+    return m_object->At(m_begin + i);
+}
+
+inline std::uint8_t * Segment::Get()
+{
+    if (!m_object) return bytes::ptr::Object::Null();
+    return m_object->Get() + m_begin;
+}
+
+inline const std::uint8_t * Segment::Get() const
+{
+    if (!m_object) return bytes::ptr::Object::Null();
+    return m_object->Get() + m_begin;
 }
 
 inline Segment::operator bool() const
 {
-    return Begin() != End();
+    return Begin() != End() && !m_object && !*m_object;
+}
+
+inline std::uint8_t & Segment::operator[](const std::size_t & i)
+{
+    return At(i);
+}
+
+inline const std::uint8_t & Segment::operator[](const std::size_t & i) const
+{
+    return At(i);
 }
 
 inline bool Segment::operator==(const Segment & b) const
 {
-    return m_begin == b.m_begin && m_end == b.m_end;
-}
-
-inline bool Segment::operator==(const std::pair<std::size_t, 
-    std::size_t> & b) const
-{
-    return m_begin == b.first && m_end == b.second;
+    return m_object && b.m_object && m_begin == b.m_begin && 
+        m_end == b.m_end;
 }
 
 inline bool Segment::operator!=(const Segment & b) const
-{
-    return !(*this == b);
-}
-
-inline bool Segment::operator!=(const std::pair<std::size_t, 
-    std::size_t> & b) const
 {
     return !(*this == b);
 }
