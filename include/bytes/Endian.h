ef7590931531e16e1161a52226366043c5d06dcb
Ananta Yudica <nta040689@gmail.com>
2019-07-23 12:49:55 +0700

    Added class Endian for handle endian of bytes

---
+++ include/bytes/Endian.h
@@ -0,0 +1,61 @@
+#ifndef BYTES_ENDIAN_H_
+#define BYTES_ENDIAN_H_
+
+#include <cstdint>
+#include <cstddef>
+
+namespace bytes
+{
+
+class Endian
+{
+public:
+    enum CategoryEnum : uint8_t
+    {
+        little = 1,
+        big = 2
+    };
+private:
+    CategoryEnum m_category;
+protected:
+    inline Endian(const CategoryEnum & c);
+public:
+    virtual ~Endian() = default;
+public:
+    inline Endian(const Endian & cpy) = delete;
+    inline Endian(Endian && mov) = delete;
+public:
+    inline Endian & operator=(const Endian & cpy) = delete;
+    inline Endian & operator=(Endian && mov) = delete;
+public:
+    virtual std::size_t At(const std::size_t & i, 
+        const std::size_t & s) const = 0;
+public:
+    inline Endian::CategoryEnum Category() const;
+public:
+    inline bool operator==(const Endian & b) const;
+    inline bool operator!=(const Endian & b) const;
+};
+
+inline Endian::Endian(const CategoryEnum & c) :
+    m_category(c)
+{}
+
+inline Endian::CategoryEnum Endian::Category() const
+{
+    return m_category;
+}
+
+inline bool Endian::operator==(const Endian & b) const
+{
+    return m_category == b.m_category;
+}
+
+inline bool Endian::operator!=(const Endian & b) const
+{
+    return !(*this == b);
+}
+
+} //!bytes
+
+#endif //!BYTES_ENDIAN_H_
