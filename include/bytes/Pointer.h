15923b8d36f07ddcb0b11b41f3288815ae38ead6
Ananta Yudica <nta040689@gmail.com>
2019-07-23 12:47:57 +0700

    Removed dependency with Counter.h Added dependencies with ptr/Object.h and ptr/segment/Wrapper.h Removed member alias type 'CounterSegmentPtrType' Removed static member function 'Null' Removed member variables 'm_ptr' and 'm_size' Updated member variable 'm_segments' Updated copy and move assignment Added member function 'IsSameObject' Removed member functions

--- include/bytes/Pointer.h
+++ include/bytes/Pointer.h
@@ -7,25 +7,20 @@
 #include <utility>
 #include <cstring>
 
-#include "ptr/Counter.h"
+#include "ptr/Object.h"
 #include "ptr/Segment.h"
+#include "ptr/Segment/Wrapper.h"
 
 namespace bytes
 {
 
 class Pointer
 {
-private:
-    typedef bytes::ptr::Counter<std::shared_ptr<bytes::ptr::Segment>>
-        CounterSegmentPtrType;
-private:
-    static inline std::uint8_t * Null();
 public:
     static inline bool IsEnd(const std::uint8_t * ptr);
 private:
-    std::uint8_t * m_ptr;
-    std::size_t m_size;
-    std::vector<CounterSegmentPtrType> m_segments;
+    std::shared_ptr<bytes::ptr::Object> m_object;
+    std::vector<std::shared_ptr<bytes::ptr::segment::Warpper>> m_segments;
 public:
     inline Pointer();
     inline Pointer(const std::size_t & s);
@@ -34,22 +29,19 @@ public:
     inline Pointer(const Pointer & cpy);
     inline Pointer(Pointer && mov);
 public:
-    inline Pointer & operator=(const Pointer & cpy) = delete;
-    inline Pointer & operator=(Pointer && mov) = delete;
+    inline Pointer & operator=(const Pointer & cpy);
+    inline Pointer & operator=(Pointer && mov);
+public:
+    inline bool IsSameObject(const Pointer & b) const;
+    inline bool 
+        IsSameObject(const std::shared_ptr<bytes::ptr::Object> & b) const;
 public:
     inline void Reallocate(const std::size_t & sz);
     inline void Reallocate(const std::size_t & sz, 
         const std::shared_ptr<bytes::ptr::Segment> & seg);
-private:
-    inline void Reallocate(const std::size_t & sz, const std::size_t & bg,
-        const std::size_t & ed);
 public:
     inline std::shared_ptr<bytes::ptr::Segment> Share(const std::size_t & bg, 
         const std::size_t & ed);
-    inline std::shared_ptr<bytes::ptr::Segment> 
-        Share(const std::shared_ptr<bytes::ptr::Segment> & seg);
-public:
-    inline void Reset(const std::shared_ptr<bytes::ptr::Segment> & seg);
 public:
     inline std::size_t Size() const;
 public:
@@ -57,18 +49,9 @@ public:
 public:
     inline std::uint8_t & At(const std::size_t & i);
     inline const std::uint8_t & At(const std::size_t & i) const;
-    inline std::uint8_t & At(const std::shared_ptr<bytes::ptr::Segment> & seg, 
-        const std::size_t & i);
-    inline const std::uint8_t & 
-        At(const std::shared_ptr<bytes::ptr::Segment> & seg,
-            const std::size_t & i) const;
 public:
     inline std::uint8_t * Get();
     inline const std::uint8_t * Get() const;
-    inline std::uint8_t * 
-        Get(const std::shared_ptr<bytes::ptr::Segment> & seg);
-    inline const std::uint8_t * 
-        Get(const std::shared_ptr<bytes::ptr::Segment> & seg) const;
 public:
     inline operator bool() const;
 public:
@@ -80,91 +63,94 @@ public:
     inline bool operator!=(const Pointer & b) const;
 };
 
-inline std::uint8_t * Pointer::Null()
-{
-    static std::uint8_t n = std::uint8_t(0);
-    return &n;
-}
-
 inline bool Pointer::IsEnd(const std::uint8_t * ptr)
 {
-    return ptr == Null();
+    return ptr == bytes::ptr::Object::Null();
 }
 
 inline Pointer::Pointer() :
-    m_ptr(nullptr),
-    m_size(0)
+    m_object(nullptr)
 {}
 
 inline Pointer::Pointer(const std::size_t & s) :
-    m_ptr(nullptr),
-    m_size(s)
+    m_object(nullptr)
 {
-    if (m_size > 0)
-        m_ptr = new std::uint8_t[m_size];
+    if (s > 0) m_object = std::make_shared<bytes::ptr::Object>(s);
 }
 
 inline Pointer::~Pointer()
 {
-    if (m_ptr) delete[] m_ptr;
-    m_size = 0;
+    m_object = nullptr;
     m_segments.clear();
 }
 
 inline Pointer::Pointer(const Pointer & cpy) :
-    m_ptr(nullptr),
-    m_size(cpy.m_size)
+    m_object(nullptr)
 {
-    if (cpy.m_ptr && m_size > 0)
-    {
-        m_ptr = new std::uint8_t[m_size];
-        std::memcpy(m_ptr, cpy.m_ptr, m_size);
-    }
+    if(cpy) m_object = std::make_shared<bytes::ptr::Object>(*(cpy.m_object));
 }
 
 inline Pointer::Pointer(Pointer && mov) :
-    m_ptr(mov.m_ptr),
-    m_size(mov.m_size),
+    m_object(mov.m_object),
     m_segments(mov.m_segments)
 {
-    mov.m_ptr = nullptr;
-    mov.m_size = 0;
+    mov.m_object = nullptr;
     mov.m_segments.clear();
+}
+
+inline Pointer & Pointer::operator=(const Pointer & cpy)
+{
+    if (cpy)
+        m_object = std::make_shared<bytes::ptr::Object>(*(cpy.m_object));
+    else
+        m_object = nullptr;
+    m_segments.clear();
+    return *this;
+}
+
+inline Pointer & Pointer::operator=(Pointer && mov)
+{
+    m_object = mov.m_object;
+    m_segments = mov.m_segments;
+    mov.m_object = nullptr;
+    mov.m_segments.clear();
+    return *this;
+}
+
+inline bool Pointer::IsSameObject(const Pointer & b) const
+{
+    return (*this) && *m_object == *b.m_object;
+}
+
+inline bool 
+    Pointer::IsSameObject(const std::shared_ptr<bytes::ptr::Object> & b) const
+{
+    return (*this) && *m_object == *b;
 }
 
 inline void Pointer::Reallocate(const std::size_t & sz)
 {
-    return Reallocate(sz, 0, m_size);
+    if (!*this) return;
+    return m_object->Reallocate(sz);
 }
 
 inline void Pointer::Reallocate(const std::size_t & sz, 
     const std::shared_ptr<bytes::ptr::Segment> & seg)
 {
-    if(!seg || !*seg) return;
-    Reallocate(sz, seg->Begin(), seg->End());
-    for(auto it = m_segments.begin(); it != m_segments.end(); ++it)
+    if(!*this || !seg || !*seg) return;
+    m_object->Reallocate(sz, seg->Begin(), seg->End());
+    for(auto it = m_segments.begin(); it != m_segments.end(); )
     {
-        if ((*it).Get() == seg) continue;
-        (*it).Get()->Resize(seg->Begin(), seg->Size(), sz);
+        if ((*it).use_count() == 1)
+        {
+            it = m_segments.erase(it);
+            continue;
+        }
+        if ((**it) == *seg)
+            (*it)->End(seg->Begin() + sz);
+        else (*it)->Resize(seg->Begin(), seg->Size(), sz);
+        ++it;
     }
-    seg->End(seg->Begin() + sz);
-}
-
-inline void Pointer::Reallocate(const std::size_t & sz, const std::size_t & bg,
-    const std::size_t & ed)
-{
-    if (m_size == sz) return;
-    const std::size_t tsz = sz + bg + (m_size - ed); 
-    std::uint8_t * nptr = new std::uint8_t[tsz];
-    if (*this)
-    {
-        std::memcpy(nptr, m_ptr, bg);
-        std::memcpy(nptr + bg, m_ptr + m_size, ed - bg);
-        std::memcpy(nptr + ed, m_ptr + m_size, (m_size - ed));
-    }
-    if (m_ptr) delete[] m_ptr;
-    m_ptr = nptr;
-    m_size = tsz;
 }
 
 inline std::shared_ptr<bytes::ptr::Segment> 
@@ -173,114 +159,48 @@ inline std::shared_ptr<bytes::ptr::Segment>
     if (!*this) return std::make_shared<bytes::ptr::Segment>();
     for(auto it = m_segments.begin(); it != m_segments.end(); ++it)
     {
-        if ((*it).Get()->Begin() == bg && (*it).Get()->End() == ed)
-            return (++(*it)).Get();
+        if ((*it)->Begin() == bg && (*it)->End() == ed)
+            return (*it);
     }
-    auto nseg = std::make_shared<bytes::ptr::Segment>(bg, ed);
+    auto nseg = std::make_shared<bytes::ptr::segment::Warpper>(bg, ed, 
+        m_object);
     m_segments.push_back(nseg);
     return nseg;
-}
-
-inline std::shared_ptr<bytes::ptr::Segment> 
-    Pointer::Share(const std::shared_ptr<bytes::ptr::Segment> & seg)
-{
-    if (!*this || seg) return std::make_shared<bytes::ptr::Segment>();
-    for(auto it = m_segments.begin(); it != m_segments.end(); ++it)
-    {
-        if ((*it).Get() == seg)
-            return (++(*it)).Get();
-    }
-    auto nseg = std::make_shared<bytes::ptr::Segment>(seg->Begin(), seg->End());
-    m_segments.push_back(nseg);
-    return nseg;
-}
-
-inline void Pointer::Reset(const std::shared_ptr<bytes::ptr::Segment> & seg)
-{
-    if (!*this) return;
-    for(auto it = m_segments.begin(); it != m_segments.end(); ++it)
-    {
-        if ((*it).Get() == seg)
-        {
-            if (!(--(*it))) m_segments.erase(it);
-            break;
-        } 
-    }
 }
 
 inline std::size_t Pointer::Size() const
 {
-    return m_size;
+    return m_object->Size();
 }
 
 inline bool Pointer::Has(const std::shared_ptr<bytes::ptr::Segment> & seg) const
 {
-    for(auto it = m_segments.begin(); it != m_segments.end(); ++it)
-    {
-        if ((*it).Get() == seg) return true;
-    }
-    return false;
+    return seg && seg->IsSameObject(m_object);
 }
 
 inline std::uint8_t & Pointer::At(const std::size_t & i)
 {
-    if (i >= m_size) return *Null();
-    return m_ptr[i];
+    return m_object->At(0);
 }
 
 inline const std::uint8_t & Pointer::At(const std::size_t & i) const
 {
-    if (i >= m_size) return *Null();
-    return m_ptr[i];
-}
-
-inline std::uint8_t & 
-    Pointer::At(const std::shared_ptr<bytes::ptr::Segment> & seg, 
-        const std::size_t & i)
-{
-    if (!seg && i < seg->Begin() && i >= seg->End() && i >= m_size) 
-        return *Null();
-    return m_ptr[seg->Begin() + i];
-}
-
-inline const std::uint8_t & 
-    Pointer::At(const std::shared_ptr<bytes::ptr::Segment> & seg, 
-        const std::size_t & i) const
-{
-    if (!seg && i < seg->Begin() && i >= seg->End() && i >= m_size) 
-        return *Null();
-    return m_ptr[seg->Begin() + i];
+     return m_object->At(0);
 }
 
 inline std::uint8_t * Pointer::Get()
 {
-    if (m_ptr) return m_ptr;
-    return Null();
+    return m_object->Get();
 }
 
 inline const std::uint8_t * Pointer::Get() const
 {
-    if (m_ptr) return m_ptr;
-    return Null();
-}
-
-inline std::uint8_t * 
-    Pointer::Get(const std::shared_ptr<bytes::ptr::Segment> & seg)
-{
-    if (seg && seg->Begin() < m_size) return m_ptr + seg->Begin();
-    return Null();
-}
-
-inline const std::uint8_t * 
-    Pointer::Get(const std::shared_ptr<bytes::ptr::Segment> & seg) const
-{
-    if (seg && seg->Begin() < m_size) return m_ptr + seg->Begin();
-    return Null();
+    return m_object->Get();
 }
 
 inline Pointer::operator bool() const
 {
-    return m_ptr != nullptr && m_size != 0;
+    return m_object && static_cast<bool>(*m_object);
 }
 
 inline std::uint8_t & Pointer::operator[](const std::size_t & i)
@@ -295,14 +215,10 @@ inline const std::uint8_t & Pointer::operator[](const std::size_t & i) const
 
 inline bool Pointer::operator==(const Pointer & b) const
 {
-    if (m_ptr == b.m_ptr) return true;
-    if (m_ptr == nullptr || b.m_ptr == nullptr || m_size != b.m_size) 
-        return false;
-    for (std::size_t i = 0; i < m_size; ++i)
-    {
-        if (m_ptr[i] != b[i]) return false;
-    }
-    return true;
+    const bool ca = static_cast<bool>(*this),
+        cb = static_cast<bool>(b);
+    return (!ca && !cb) || (ca && cb && m_object == b.m_object &&
+        m_segments == b.m_segments);
 }
 
 inline bool Pointer::operator!=(const Pointer & b) const
