02415a24364e4baeb8ca6d7214d0c6f221a9c1a1
Ananta Yudica <nta040689@gmail.com>
2019-07-26 11:30:59 +0700

    Added abstract class Modifier for base implement modifier value traits of bytes

---
+++ include/bytes/trait/Modifier.h
@@ -0,0 +1,47 @@
+#ifndef BYTES_TRAIT_MODIFIER_H_
+#define BYTES_TRAIT_MODIFIER_H_
+
+#include <cstdint>
+#include <cstddef>
+#include <memory>
+
+namespace bytes
+{
+namespace trait
+{
+
+class Modifier
+{
+protected:
+    Modifier() = default;
+public:
+    virtual ~Modifier() = default;
+protected:
+    Modifier(const Modifier & cpy) = default;
+    Modifier(Modifier && mov) = default;
+protected:
+    inline Modifier & operator=(const Modifier & cpy) = default;
+    inline Modifier & operator=(Modifier && mov) = default;
+public:
+    virtual inline std::shared_ptr<Modifier> Copy() = 0;
+public:
+    virtual inline std::shared_ptr<Modifier> Move() = 0;
+public:
+    virtual std::uint8_t OnSet(const std::uint8_t & v, 
+        const std::size_t & i, const std::size_t & bg,
+        const std::size_t & ed) const = 0;
+public:
+    virtual std::uint8_t OnGet(const std::uint8_t & v, 
+        const std::size_t & i, const std::size_t & bg,
+        const std::size_t & ed) const = 0;
+public:
+    virtual inline bool operator==(const Modifier & b) const = 0;
+public:
+    virtual inline bool operator!=(const Modifier & b) const = 0;
+};
+
+} //!trait
+
+} //!bytes
+
+#endif //!BYTES_TRAIT_MODIFIER_H_
