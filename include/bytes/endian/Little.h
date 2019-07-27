9b7f27783291baa23ea415bb9ccb371dcdada57a
Ananta Yudica <nta040689@gmail.com>
2019-07-27 07:36:30 +0700

    Fixed return value of function At Updated member function 'At' Added member function 'Begin' and 'End'

--- include/bytes/endian/Little.h
+++ include/bytes/endian/Little.h
@@ -24,7 +24,14 @@ public:
     inline Little & operator=(const Little & cpy) = delete;
     inline Little & operator=(Little && mov) = delete;
 public:
-    inline std::size_t At(const std::size_t & i, const std::size_t & s) const;
+    inline std::size_t At(const std::size_t & i, const std::size_t & bg, 
+        const std::size_t & ed) const;
+public:
+    inline std::size_t Begin(const std::size_t & bg, 
+        const std::size_t & ed) const;
+public:
+    inline std::size_t End(const std::size_t & bg, 
+        const std::size_t & ed) const;
 };
 
 inline Little & Little::Instance()
@@ -37,9 +44,22 @@ inline Little::Little() :
     bytes::Endian(bytes::Endian::little)
 {}
 
-inline std::size_t Little::At(const std::size_t & i, const std::size_t & s) const
+inline std::size_t Little::At(const std::size_t & i, const std::size_t & bg, 
+        const std::size_t & ed) const
 {
-    return i % s;
+    return bg + i;
+}
+
+inline std::size_t Little::Begin(const std::size_t & bg, 
+    const std::size_t & ed) const
+{
+    return bg;
+}
+
+inline std::size_t Little::End(const std::size_t & bg, 
+    const std::size_t & ed) const
+{
+    return ed;
 }
 
 } //!endian
