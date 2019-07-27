b8d91084bd6fdb603610185d8c42d6928234c06c
Ananta Yudica <nta040689@gmail.com>
2019-07-27 10:54:36 +0700

    Added dependencies with Endian.h and Trait.h Added static member function 'GetTrait' Added member variable 'm_trait' Updated construction Added member function 'Resize' Updated member function operator=, 'At', and operator==

--- include/bytes/ptr/Segment.h
+++ include/bytes/ptr/Segment.h
@@ -6,6 +6,8 @@
 #include <memory>
 
 #include "Object.h"
+#include "../Endian.h"
+#include "../Trait.h"
 
 namespace bytes
 {
@@ -15,13 +17,21 @@ namespace ptr
 class Segment
 {
 private:
+    static inline std::shared_ptr<bytes::Trait> GetTrait();
+    static inline std::shared_ptr<bytes::Trait> 
+        GetTrait(const std::shared_ptr<bytes::Trait> & t);
+private:
     std::size_t m_begin;
     std::size_t m_end;
     std::shared_ptr<bytes::ptr::Object> m_object;
+    std::shared_ptr<bytes::Trait> m_trait;
 public:
     inline Segment();
     inline Segment(const std::size_t & bg, const std::size_t & ed,
         const std::shared_ptr<bytes::ptr::Object> & o);
+    inline Segment(const std::size_t & bg, const std::size_t & ed,
+        const std::shared_ptr<bytes::ptr::Object> & o,
+        const std::shared_ptr<bytes::Trait> & t);
     inline ~Segment();
 public:
     inline Segment(const Segment & cpy);
@@ -43,6 +53,8 @@ protected:
     inline void End(const std::size_t & ed);
 public:
     inline std::size_t Size() const;
+protected:
+    inline std::size_t Resize(const std::size_t & s);
 public:
     inline std::uint8_t & At(const std::size_t & i);
     inline const std::uint8_t & At(const std::size_t & i) const;
@@ -60,17 +72,40 @@ public:
     inline bool operator!=(const Segment & b) const;
 };
 
+inline std::shared_ptr<bytes::Trait> Segment::GetTrait()
+{
+    return std::make_shared<bytes::Trait>();
+}
+
+inline std::shared_ptr<bytes::Trait> 
+    Segment::GetTrait(const std::shared_ptr<bytes::Trait> & t)
+{
+    if (t) return t;
+    return GetTrait();
+}
+    
 inline Segment::Segment() :
     m_begin(0),
     m_end(0),
-    m_object(nullptr)
+    m_object(nullptr),
+    m_trait(nullptr)
 {}
 
 inline Segment::Segment(const std::size_t & bg, const std::size_t & ed,
     const std::shared_ptr<bytes::ptr::Object> & o) :
         m_begin(bg),
         m_end(ed),
-        m_object(o)
+        m_object(o),
+        m_trait(GetTrait())
+{}
+
+inline Segment::Segment(const std::size_t & bg, const std::size_t & ed,
+    const std::shared_ptr<bytes::ptr::Object> & o,
+    const std::shared_ptr<bytes::Trait> & t) :
+        m_begin(bg),
+        m_end(ed),
+        m_object(o),
+        m_trait(GetTrait(t))
 {}
 
 inline Segment::~Segment()
@@ -78,31 +113,46 @@ inline Segment::~Segment()
     m_begin = 0;
     m_end = 0;
     m_object = nullptr;
+    m_trait = nullptr;
 }
 
 inline Segment::Segment(const Segment & cpy) :
-    m_begin(cpy.m_begin),
-    m_end(cpy.m_end),
-    m_object(std::make_shared<bytes::ptr::Object>(*(cpy.m_object), 
-        m_begin, m_end))
-{}
+    m_begin(0),
+    m_end(cpy.Size()),
+    m_object(nullptr),
+    m_trait(nullptr)
+{
+    if (cpy.m_object)
+        m_object = std::make_shared<bytes::ptr::Object>(*(cpy.m_object), 
+            m_begin, m_end);
+    if (cpy.m_trait) m_trait = std::make_shared<bytes::Trait>(*(cpy.m_trait));
+    else m_trait = GetTrait();
+        
+}
 
 inline Segment::Segment(Segment && mov) :
     m_begin(mov.m_begin),
     m_end(mov.m_end),
-    m_object(mov.m_object)
+    m_object(mov.m_object),
+    m_trait(mov.m_trait)
 {
     mov.m_begin = 0;
     mov.m_end = 0;
     mov.m_object = nullptr;
+    mov.m_trait = nullptr;
 }
 
 inline Segment & Segment::operator=(const Segment & cpy)
 {
-    m_begin = cpy.m_begin;
-    m_end = cpy.m_end;
-    m_object = std::make_shared<bytes::ptr::Object>(*(cpy.m_object),
-        m_begin, m_end);
+    m_begin = 0;
+    m_end = cpy.Size();
+    m_object = nullptr;
+    m_trait = nullptr;
+    if (cpy.m_object) 
+        m_object = std::make_shared<bytes::ptr::Object>(*(cpy.m_object),
+            m_begin, m_end);
+    if (cpy.m_trait) m_trait = std::make_shared<bytes::Trait>(*(cpy.m_trait));
+    else m_trait = GetTrait();
     return *this;
 }
 
@@ -111,9 +161,11 @@ inline Segment & Segment::operator=(Segment && mov)
     m_begin = mov.m_begin;
     m_end = mov.m_end;
     m_object = mov.m_object;
+    m_trait = mov.m_trait;
     mov.m_begin = 0;
     mov.m_end = 0;
     mov.m_object = nullptr;
+    mov.m_trait = nullptr;
     return *this;
 }
 
@@ -121,6 +173,7 @@ inline bool Segment::IsSameObject(const Segment & seg) const
 {
     return seg.m_object == m_object;
 }
+
 inline bool Segment::
     IsSameObject(const std::shared_ptr<bytes::ptr::Object> & b) const
 {
@@ -152,16 +205,26 @@ inline std::size_t Segment::Size() const
     return (m_end > m_begin ? m_end - m_begin : 0);
 }
 
+inline void Segment::Resize(const std::size_t & s)
+{
+    const std::size_t cs = Size();
+    if (cs == s) return cs;
+    const std::size_t ns = m_trait->Resize(cs, s);
+    if (cs == ns) return ns;
+    m_end = m_begin + ns;
+    return ns;
+}
+
 inline std::uint8_t & Segment::At(const std::size_t & i)
 {
-    if (!m_object) return *bytes::ptr::Object::Null();
-    return m_object->At(m_begin + i);
+    if (!m_object || !m_trait) return *bytes::ptr::Object::Null();
+    return m_object->At(m_trait->At(i, m_begin, m_end));
 }
 
 inline const std::uint8_t & Segment::At(const std::size_t & i) const
 {
-    if (!m_object) return *bytes::ptr::Object::Null();
-    return m_object->At(m_begin + i);
+    if (!m_object || !m_trait) return *bytes::ptr::Object::Null();
+    return m_object->At(m_trait->At(i, m_begin, m_end));
 }
 
 inline std::uint8_t * Segment::Get()
@@ -196,7 +259,7 @@ inline bool Segment::operator==(const Segment & b) const
     const bool ca = static_cast<bool>(*this),
         cb = static_cast<bool>(b);
     return (!ca && !cb) || (ca && cb && m_begin == b.m_begin &&
-        m_end == b.m_end);
+        m_end == b.m_end && m_trait == b.m_trait);
 }
 
 inline bool Segment::operator!=(const Segment & b) const
