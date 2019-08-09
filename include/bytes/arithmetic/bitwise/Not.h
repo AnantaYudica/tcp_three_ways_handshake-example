c682df651ce716b33f7cd617bd1fe564bb77c71c
Ananta Yudica <nta040689@gmail.com>
2019-08-10 01:32:29 +0700

    Added class bitwise::Not for handle bitwise not of Bytes

---
+++ include/bytes/arithmetic/bitwise/Not.h
@@ -0,0 +1,41 @@
+#ifndef BYTES_ARITHMETIC_BITWISE_NOT_H_
+#define BYTES_ARITHMETIC_BITWISE_NOT_H_
+
+#include <cstddef>
+#include <memory>
+
+#include "../../ptr/Segment.h"
+
+namespace bytes
+{
+namespace arithmetic
+{
+namespace bitwise
+{
+
+class Not
+{
+public:
+    typedef bytes::ptr::Segment SegmentType;
+    typedef std::shared_ptr<SegmentType> SegmentPtrType;
+public:
+    static inline void Operator(SegmentPtrType segment);
+private:
+    inline Not() = delete;
+public:
+    inline ~Not() = delete;
+};
+
+inline void Not::Operator(SegmentPtrType segment)
+{
+    for (std::size_t i = 0; i < segment->Size(); ++i)
+        segment->At(i) = ~segment->At(i);
+}
+
+} //!bitwise
+
+} //!arithmetic
+
+} //!bytes
+
+#endif //!BYTES_ARITHMETIC_BITWISE_NOT_H_
