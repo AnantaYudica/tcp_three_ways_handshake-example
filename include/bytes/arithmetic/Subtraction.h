ece3818302b201eb5b5df912c3354e68229b4325
Ananta Yudica <nta040689@gmail.com>
2019-08-11 00:43:40 +0700

    Added class arithmetic::Subtraction for handle arithmetic subtraction of Bytes

---
+++ include/bytes/arithmetic/Subtraction.h
@@ -0,0 +1,101 @@
+#ifndef BYTES_ARITHMETIC_SUBTRACTION_H_
+#define BYTES_ARITHMETIC_SUBTRACTION_H_
+
+#include <cstdint>
+#include <cstddef>
+#include <memory>
+
+#include "../sys/Endian.h"
+#include "../ptr/Segment.h"
+
+namespace bytes
+{
+namespace arithmetic
+{
+
+class Subtraction
+{
+public:
+    typedef bytes::ptr::Segment SegmentType;
+    typedef const SegmentType ConstSegmentType;
+    typedef std::shared_ptr<SegmentType> SegmentPtrType;
+    typedef std::shared_ptr<ConstSegmentType> ConstSegmentPtrType;
+public:
+    static inline void Operator(ConstSegmentPtrType a_segment, 
+        ConstSegmentPtrType b_segment, SegmentPtrType result_segment);
+    static inline void Operator(SegmentPtrType a_segment, 
+        ConstSegmentPtrType b_segment, SegmentPtrType result_segment);
+    static inline void Operator(ConstSegmentPtrType a_segment, 
+        SegmentPtrType b_segment, SegmentPtrType result_segment);
+    static inline void Operator(SegmentPtrType a_segment, 
+        SegmentPtrType b_segment, SegmentPtrType result_segment);
+
+    static inline void Operator(SegmentPtrType a_result_segment, 
+        ConstSegmentPtrType b_segment);
+    static inline void Operator(SegmentPtrType a_result_segment, 
+        SegmentPtrType b_segment);
+private:
+    inline Subtraction() = delete;
+public:
+    inline ~Subtraction() = delete;
+};
+
+inline void Subtraction::Operator(ConstSegmentPtrType a_segment, 
+    ConstSegmentPtrType b_segment, SegmentPtrType result_segment)
+{
+    const auto & endian = bytes::sys::Endian();
+    std::uint8_t c = std::uint8_t(0);
+    std::size_t i = 0;
+    for (; i < result_segment->Size(); ++i)
+    {
+        std::uint16_t val = 0;
+        if (a_segment->IsEnd(i))
+            val += a_segment->At(i);
+        if (b_segment->IsEnd(i))
+            val -= b_segment->At(i);
+        val -= c;
+        std::uint8_t * pval = reinterpret_cast<std::uint8_t *>(&val);
+        c = (~pval[endian.At(1, 0, 2)] + std::uint8_t(1));
+        result_segment->At(i) = pval[endian.At(0, 0, 2)];
+    }
+}
+
+inline void Subtraction::Operator(SegmentPtrType a_segment, 
+    ConstSegmentPtrType b_segment, SegmentPtrType result_segment)
+{
+    Subtraction::Operator(std::const_pointer_cast<ConstSegmentType>(a_segment),
+        b_segment, result_segment);
+}
+
+inline void Subtraction::Operator(ConstSegmentPtrType a_segment, 
+    SegmentPtrType b_segment, SegmentPtrType result_segment)
+{
+    Subtraction::Operator(a_segment,
+        std::const_pointer_cast<ConstSegmentType>(b_segment), result_segment);
+}
+
+inline void Subtraction::Operator(SegmentPtrType a_segment, 
+    SegmentPtrType b_segment, SegmentPtrType result_segment)
+{
+    Subtraction::Operator(std::const_pointer_cast<ConstSegmentType>(a_segment),
+        std::const_pointer_cast<ConstSegmentType>(b_segment), result_segment);
+}
+
+inline void Subtraction::Operator(SegmentPtrType a_result_segment, 
+    ConstSegmentPtrType b_segment)
+{
+    Subtraction::Operator(a_result_segment, b_segment, a_result_segment);
+}
+
+inline void Subtraction::Operator(SegmentPtrType a_result_segment, 
+    SegmentPtrType b_segment)
+{
+    Subtraction::Operator(a_result_segment, 
+        std::const_pointer_cast<ConstSegmentType>(b_segment));
+}
+
+} //!arithmetic
+
+} //!bytes
+
+#endif //!BYTES_ARITHMETIC_SUBTRACTION_H_
