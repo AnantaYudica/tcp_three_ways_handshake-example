7a2e6d8a61c389a64452f6bd774e1b81b1bde8c1
Ananta Yudica <nta040689@gmail.com>
2019-08-17 22:04:47 +0700

    Updated overloading function with std::uint8_t *, std::uint8_t[] and std::uint8_t as parameter

--- include/bytes/arithmetic/Multiplication.h
+++ include/bytes/arithmetic/Multiplication.h
@@ -43,6 +43,37 @@ public:
         SegmentPtrType a_result_segment, ConstSegmentPtrType b_segment);
     static inline void Operator(PointerPtrType a_result_ptr,
         SegmentPtrType a_result_segment, SegmentPtrType b_segment);
+public:
+    static inline void Operator(ConstSegmentPtrType a_segment,
+        const std::uint8_t * b, const std::size_t & b_size, 
+        PointerPtrType result_ptr, SegmentPtrType result_segment);
+    static inline void Operator(SegmentPtrType a_segment,
+        const std::uint8_t * b, const std::size_t & b_size, 
+        PointerPtrType result_ptr, SegmentPtrType result_segment);
+    static inline void Operator(PointerPtrType a_result_ptr,
+        SegmentPtrType a_result_segment,
+        const std::uint8_t * b, const std::size_t & b_size);
+public:
+    template<std::size_t N>
+    static inline void Operator(ConstSegmentPtrType a_segment,
+        const std::uint8_t (&b)[N], PointerPtrType result_ptr, 
+        SegmentPtrType result_segment);
+    template<std::size_t N>
+    static inline void Operator(SegmentPtrType a_segment,
+        const std::uint8_t (&b)[N], PointerPtrType result_ptr, 
+        SegmentPtrType result_segment);
+    template<std::size_t N>
+    static inline void Operator(PointerPtrType a_result_ptr,
+        SegmentPtrType a_result_segment, const std::uint8_t (&b)[N]);
+public:
+    static inline void Operator(ConstSegmentPtrType a_segment,
+        const std::uint8_t & b, PointerPtrType result_ptr, 
+        SegmentPtrType result_segment);
+    static inline void Operator(SegmentPtrType a_segment,
+        const std::uint8_t & b, PointerPtrType result_ptr, 
+        SegmentPtrType result_segment);
+    static inline void Operator(PointerPtrType a_result_ptr,
+        SegmentPtrType a_result_segment, const std::uint8_t & b);
 private:
     inline Multiplication() = delete;
 public:
@@ -141,7 +172,125 @@ inline void Multiplication::Operator(PointerPtrType a_result_ptr,
 
 inline void Multiplication::Operator(PointerPtrType a_result_ptr,
     SegmentPtrType a_result_segment, SegmentPtrType b_segment)
-{}
+{
+    Multiplication::Operator(a_result_ptr, a_result_segment,
+        std::const_pointer_cast<ConstSegmentType>(b_segment));
+}
+
+
+inline void Multiplication::Operator(ConstSegmentPtrType a_segment,
+    const std::uint8_t * b, const std::size_t & b_size, 
+    PointerPtrType result_ptr, SegmentPtrType result_segment)
+{
+    const auto & endian = bytes::sys::Endian();
+    const auto a_size = a_segment->Size();
+    const auto result_size = result_segment->Size();
+    bytes::Pointer mul_ptr{result_size};
+    auto mul_trait = std::make_shared<bytes::Trait>(result_segment->Trait());
+    auto mul_segment = mul_ptr.Share(0, result_size, mul_trait);
+    for (std::size_t i = 0; i < b_size; ++i)
+    {
+        bytes::Assign::Operator(mul_segment, std::uint8_t(0));
+        std::uint16_t carry = 0;
+        std::uint8_t * carry_ptr = reinterpret_cast<std::uint8_t *>(&carry);
+        std::size_t j;
+        for (j = 0; j < a_size; ++j)
+        {
+            if (b[i] == 0 && carry == 0) break;
+            std::uint16_t val = a_segment->At(j);
+            val *= b[i];
+            std::uint8_t * pval = reinterpret_cast<std::uint8_t *>(&val);
+            carry += pval[endian.At(0, 0, 2)];
+            mul_segment->At(j) = carry_ptr[endian.At(0, 0, 2)];
+            carry >>= sizeof(std::uint8_t) * 8;
+            carry += pval[endian.At(1, 0, 2)];
+        }
+        const std::size_t resize = (carry_ptr[endian.At(1, 0, 2)] != 0 ? 2 :
+            (carry_ptr[endian.At(0, 0, 2)] != 0 ? 1 : 0));
+        if (resize > 0)
+            mul_ptr.Reallocate(mul_segment->Size() + resize, mul_segment);
+        mul_segment->At(j++) = carry_ptr[endian.At(0, 0, 2)];
+        mul_segment->At(j) = carry_ptr[endian.At(1, 0, 2)];
+        bytes::arithmetic::bitwise::Shift::Operator(mul_segment, 8 * i);
+        bytes::arithmetic::Addition::Operator(result_ptr, result_segment, 
+            mul_segment);
+    }
+}
+
+inline void Multiplication::Operator(SegmentPtrType a_segment,
+    const std::uint8_t * b, const std::size_t & b_size, 
+    PointerPtrType result_ptr, SegmentPtrType result_segment)
+{
+    Multiplication::Operator(
+        std::const_pointer_cast<ConstSegmentType>(a_segment), b, b_size,
+        result_ptr, result_segment);
+}
+
+inline void Multiplication::Operator(PointerPtrType a_result_ptr,
+    SegmentPtrType a_result_segment,
+    const std::uint8_t * b, const std::size_t & b_size)
+{
+    const std::size_t expand_size = a_result_segment->Size() + b_size;
+    auto result_ptr = std::make_shared<bytes::Pointer>(expand_size);
+    auto result_trait = 
+        std::make_shared<bytes::Trait>(a_result_segment->Trait());
+    auto result_segment = result_ptr->Share(0, expand_size, result_trait);
+    bytes::Assign::Operator(result_segment, std::uint8_t(0));
+    Multiplication::Operator(a_result_segment, b, b_size, result_ptr, 
+        result_segment);
+    std::size_t reduce_size = 0;
+    for (std::size_t i = result_segment->Size() - 1; 
+        i < result_segment->Size(); --i, ++reduce_size)
+    {
+        if (result_segment->At(i) != std::uint8_t(0)) break;
+    }
+    bytes::Assign::Operator(result_ptr, a_result_segment, 0, 
+        result_segment, 0, result_segment->Size() - reduce_size);
+}
+
+template<std::size_t N>
+inline void Multiplication::Operator(ConstSegmentPtrType a_segment,
+    const std::uint8_t (&b)[N], PointerPtrType result_ptr, 
+    SegmentPtrType result_segment)
+{
+    Multiplication::Operator(a_segment, b, N, result_ptr, result_segment);
+}
+
+template<std::size_t N>
+inline void Multiplication::Operator(SegmentPtrType a_segment,
+    const std::uint8_t (&b)[N], PointerPtrType result_ptr, 
+    SegmentPtrType result_segment)
+{
+    Multiplication::Operator(a_segment, b, N, result_ptr, result_segment);
+}
+
+template<std::size_t N>
+inline void Multiplication::Operator(PointerPtrType a_result_ptr,
+    SegmentPtrType a_result_segment, const std::uint8_t (&b)[N])
+{
+    Multiplication::Operator(a_result_ptr, a_result_segment, b, N);
+}
+
+inline void Multiplication::Operator(ConstSegmentPtrType a_segment,
+    const std::uint8_t & b, PointerPtrType result_ptr, 
+    SegmentPtrType result_segment)
+{
+    Multiplication::Operator(a_segment, &b, 1, result_ptr, result_segment);
+}
+
+inline void Multiplication::Operator(SegmentPtrType a_segment,
+    const std::uint8_t & b, PointerPtrType result_ptr, 
+    SegmentPtrType result_segment)
+{
+    Multiplication::Operator(a_segment, &b, 1, result_ptr, result_segment);
+}
+
+inline void Multiplication::Operator(PointerPtrType a_result_ptr,
+    SegmentPtrType a_result_segment, const std::uint8_t & b)
+{
+    
+    Multiplication::Operator(a_result_ptr, a_result_segment, &b, 1);
+}
 
 } //!arithmetic
 
