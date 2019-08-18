1a26b99b40fae68360b8eca3cade054a658e29af
Ananta Yudica <nta040689@gmail.com>
2019-08-18 19:25:44 +0700

    Updated test of Bit

--- test/Bit.main.cpp
+++ test/Bit.main.cpp
@@ -36,14 +36,30 @@ int main()
     assert(static_cast<bool>(b0_2) == true);
     assert(static_cast<std::uint8_t>(b0_2) == std::uint8_t(1));
 
-    std::shared_ptr<std::uint8_t> share0 = std::make_shared<std::uint8_t>(0x10);
-    std::shared_ptr<std::uint8_t> share1 = std::make_shared<std::uint8_t>(0x00);
-    std::shared_ptr<std::uint8_t> share2 = std::make_shared<std::uint8_t>(0x00);
-    std::shared_ptr<std::uint8_t> share3 = std::make_shared<std::uint8_t>(0x00);
-    Bit b1{share0, 4};
-    Bit b1_0{share1, 4};
-    Bit b1_2{share2, 4};
-    Bit b1_3{share3, 4};
+    std::shared_ptr<bytes::ptr::Object> share0 = 
+        std::make_shared<bytes::ptr::Object>(1);
+    share0->At(0) = 0x10;
+    std::shared_ptr<bytes::ptr::Segment> seg0 =
+        std::make_shared<bytes::ptr::Segment>(0, 1, share0);
+    std::shared_ptr<bytes::ptr::Object> share1 = 
+        std::make_shared<bytes::ptr::Object>(1);
+    share1->At(0) = 0x00;
+    std::shared_ptr<bytes::ptr::Segment> seg1 =
+        std::make_shared<bytes::ptr::Segment>(0, 1, share1);
+    std::shared_ptr<bytes::ptr::Object> share2 = 
+        std::make_shared<bytes::ptr::Object>(1);
+    share2->At(0) = 0x00;
+    std::shared_ptr<bytes::ptr::Segment> seg2 =
+        std::make_shared<bytes::ptr::Segment>(0, 1, share2);
+    std::shared_ptr<bytes::ptr::Object> share3 = 
+        std::make_shared<bytes::ptr::Object>(1);
+    share3->At(0) = 0x00;
+    std::shared_ptr<bytes::ptr::Segment> seg3 =
+        std::make_shared<bytes::ptr::Segment>(0, 1, share3);
+    Bit b1{seg0, 4};
+    Bit b1_0{seg1, 4};
+    Bit b1_2{seg2, 4};
+    Bit b1_3{seg3, 4};
     
     assert(static_cast<bool>(b1) == true);
     assert(static_cast<std::uint8_t>(b1) == std::uint8_t(1));
@@ -56,18 +72,18 @@ int main()
     b1_0 |= true;
     assert(static_cast<bool>(b1_0) == true);
     assert(static_cast<std::uint8_t>(b1_0) == std::uint8_t(1));
-    assert(*share1 == std::uint8_t(0x10));
+    assert(share1->At(0) == std::uint8_t(0x10));
 
     b1_2 = true;
     b1_2 &= true;
     assert(static_cast<bool>(b1_2) == true);
     assert(static_cast<std::uint8_t>(b1_2) == std::uint8_t(1));
-    assert(*share2 == std::uint8_t(0x10));
+    assert(share2->At(0) == std::uint8_t(0x10));
 
     b1_3 ^= true;
     assert(static_cast<bool>(b1_3) == true);
     assert(static_cast<std::uint8_t>(b1_3) == std::uint8_t(1));
-    assert(*share3 == std::uint8_t(0x10));
+    assert(share3->At(0) == std::uint8_t(0x10));
 
     return 0;
 }
