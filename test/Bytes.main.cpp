6b5058539750a288ba617b0e1cf53d2cf64f936e
Ananta Yudica <nta040689@gmail.com>
2019-08-23 15:38:13 +0700

    Updated test

--- test/Bytes.main.cpp
+++ test/Bytes.main.cpp
@@ -3,8 +3,9 @@
 
 int main()
 {
-    Bytes a0({0x01, 0x02, 0x03, 0x04});
+    Bytes a0({0x01, 0x02, 0x03, 0x04}, bytes::Endian::big);
     bytes::Trait trait0 {
+        bytes::Endian::big,
         std::make_shared<bytes::trait::capacity::Fixed>(),
         std::make_shared<bytes::trait::element::Direct>(),
         std::make_shared<bytes::trait::modifier::Direct>()};
@@ -36,33 +37,47 @@ int main()
     assert(a0[0] == std::uint8_t(0x55));
     assert(a0[1] == std::uint8_t(0x33));
     a0_2 >>= 5;
-    assert(a0[2] == std::uint8_t(0x11));
-    assert(a0[3] == std::uint8_t(0x01));
+    assert(a0[2] == std::uint8_t(0x01));
+    assert(a0[3] == std::uint8_t(0x11));
     a0_1 <<= 9;
-    assert(a0[0] == std::uint8_t(0x00));
-    assert(a0[1] == std::uint8_t(0xAA));
-    a0_2 += Bytes{{0xFF, 0x0E}};
+    assert(a0[0] == std::uint8_t(0x66));
+    assert(a0[1] == std::uint8_t(0x00));
+    a0_2 += Bytes{{0x0E, 0xFF}};
     assert(a0[2] == std::uint8_t(0x10));
     assert(a0[3] == std::uint8_t(0x10));
     a0_1 -= Bytes{{0x11, 0x01}};
-    assert(a0[0] == std::uint8_t(0xEF));
-    assert(a0[1] == std::uint8_t(0xA8));
+    assert(a0[0] == std::uint8_t(0x54));
+    assert(a0[1] == std::uint8_t(0xFF));
     a0_2 *= Bytes{{0x0E, 0x0E}};
-    assert(a0[2] == std::uint8_t(0xE0));
-    assert(a0[3] == std::uint8_t(0xC0));
+    assert(a0[2] == std::uint8_t(0xC0));
+    assert(a0[3] == std::uint8_t(0xE0));
     a0_1 /= Bytes{{0x0A}};
-    assert(a0[0] == std::uint8_t(0xE4));
-    assert(a0[1] == std::uint8_t(0x10));
+    assert(a0[0] == std::uint8_t(0x08));
+    assert(a0[1] == std::uint8_t(0x7F));
     a0_2 %= Bytes{{0x11, 0x11}};
-    assert(a0[2] == std::uint8_t(0x25));
-    assert(a0[3] == std::uint8_t(0x05));
-    Bytes a1{0x01, 0x02};
-    Bytes a2{0x02, 0x01};
-    Bytes a3{0x01, 0x02, 0x03, 0x04};
+    assert(a0[2] == std::uint8_t(0x05));
+    assert(a0[3] == std::uint8_t(0x25));
+    Bytes a1{{0x01, 0x02}};
+    Bytes a2{{0x02, 0x01}};
+    Bytes a3{{0x01, 0x02, 0x03, 0x04}, bytes::Endian::big};
     auto a3_0 = a3.Slice(0, 2);
     auto a3_1 = a3.Slice(2, 2);
     assert(a1 != a2);
     assert(a1 == a3_0);
     assert(a2 <= a3_1);
+
+    Bytes a11({0x01, 0x02, 0x03, 0x04}, bytes::Endian::little);
+    bytes::Trait trait11 {
+        bytes::Endian::little,
+        std::make_shared<bytes::trait::capacity::Fixed>(),
+        std::make_shared<bytes::trait::element::Direct>(),
+        std::make_shared<bytes::trait::modifier::Direct>()};
+    auto a11_1 = a11.Slice(0, 2, trait11);
+    auto a11_2 = a11.Slice(2, 2, trait11);
+    assert(a11_1[0] == std::uint8_t(0x03));
+    assert(a11_1[1] == std::uint8_t(0x04));
+    assert(a11_2[0] == std::uint8_t(0x01));
+    assert(a11_2[1] == std::uint8_t(0x02));
+
     return 0;
 }
