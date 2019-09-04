e81a32f8d9b79d67fa2028a84eaf1c6d8d566417
Ananta Yudica <nta040689@gmail.com>
2019-09-04 08:06:48 +0700

    Updated parameter abstract member functions

--- include/bytes/Endian.h
+++ include/bytes/Endian.h
@@ -41,19 +41,20 @@ public:
         const std::size_t & ed) const = 0;
 public:
     virtual std::uint8_t ValueAt(const std::uint8_t & v_at_0, 
-        const std::uint8_t & v_at_1, const std::uint8_t & off) const = 0;
+        const std::uint8_t & v_at_1, const std::uint8_t & off,
+        const std::uint8_t & sz) const = 0;
 public:
     virtual void ValueAt(std::uint8_t & v_at_0, 
         std::uint8_t & v_at_1, const std::uint8_t & v_in, 
-        const std::uint8_t & off) const = 0;
+        const std::uint8_t & off, const std::uint8_t & sz) const = 0;
 public:
     virtual std::uint8_t ValueReverseAt(const std::uint8_t & v_reverse_at_0, 
         const std::uint8_t & v_reverse_at_1, 
-        const std::uint8_t & off) const = 0;
+        const std::uint8_t & off, const std::uint8_t & sz) const = 0;
 public:
     virtual void ValueReverseAt(std::uint8_t & v_reverse_at_0, 
         std::uint8_t & v_reverse_at_1, const std::uint8_t & v_in, 
-        const std::uint8_t & off) const = 0;
+        const std::uint8_t & off, const std::uint8_t & sz) const = 0;
 public:
     virtual void Copy(std::uint8_t * a, const std::size_t & as,
         const std::uint8_t * b, const std::size_t & bs) const = 0;
