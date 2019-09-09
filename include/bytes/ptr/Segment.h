2783cc6fb873377b2d857ff8cfae1e5b3b7523b9
Ananta Yudica <nta040689@gmail.com>
2019-09-09 12:09:52 +0700

    Updated for handle offset Removed At and ReverseAt for handle offset

--- include/bytes/ptr/Segment.h
+++ include/bytes/ptr/Segment.h
@@ -23,6 +23,7 @@ private:
     static inline std::shared_ptr<bytes::Trait> 
         Trait(const std::shared_ptr<bytes::Trait> & t);
 private:
+    std::uint8_t m_offset;
     std::size_t m_begin;
     std::size_t m_end;
     std::shared_ptr<bytes::ptr::Object> m_object;
@@ -34,6 +35,14 @@ public:
     inline Segment(const std::size_t & bg, const std::size_t & ed,
         const std::shared_ptr<bytes::ptr::Object> & o,
         const std::shared_ptr<bytes::Trait> & t);
+    inline Segment(const std::size_t & bg, const std::size_t & ed,
+        const std::uint8_t & off, 
+        const std::shared_ptr<bytes::ptr::Object> & o);
+    inline Segment(const std::size_t & bg, const std::size_t & ed,
+        const std::uint8_t & off, 
+        const std::shared_ptr<bytes::ptr::Object> & o,
+        const std::shared_ptr<bytes::Trait> & t);
+public:
     inline ~Segment();
 public:
     inline Segment(const Segment & cpy);
@@ -48,6 +57,10 @@ public:
     inline bool 
         IsSameObject(const std::shared_ptr<bytes::ptr::Object> & b) const;
 public:
+    inline std::uint8_t Offset() const;
+protected:
+    inline void Offset(const std::uint8_t & off);
+public:
     inline std::size_t Begin() const;
 protected:
     inline void Begin(const std::size_t & bg);
@@ -58,28 +71,20 @@ protected:
 public:
     inline std::size_t Size() const;
 protected:
-    inline std::size_t Resize(const std::size_t & s);
+    inline std::size_t Reallocate(const std::size_t & s);
 public:
     inline bytes::Element At(const std::size_t & i);
-    inline bytes::Element At(const std::size_t & i, const std::uint8_t & off);
-    inline bytes::Element At(const std::size_t & i, const std::uint8_t & off,
-        const std::uint8_t & sz);
+    inline bytes::Element At(const std::size_t & i, const std::uint8_t & sz);
     inline const bytes::Element At(const std::size_t & i) const;
     inline const bytes::Element At(const std::size_t & i, 
-        const std::uint8_t & off) const;
-    inline const bytes::Element At(const std::size_t & i, 
-        const std::uint8_t & off, const std::uint8_t & sz) const;
+        const std::uint8_t & sz) const;
 public:
     inline bytes::ReverseElement ReverseAt(const std::size_t & i);
     inline bytes::ReverseElement ReverseAt(const std::size_t & i, 
-        const std::uint8_t & off);
-    inline bytes::ReverseElement ReverseAt(const std::size_t & i, 
-        const std::uint8_t & off, const std::uint8_t & sz);
+        const std::uint8_t & sz);
     inline const bytes::ReverseElement ReverseAt(const std::size_t & i) const;
     inline const bytes::ReverseElement ReverseAt(const std::size_t & i,
-        const std::uint8_t & off) const;
-    inline const bytes::ReverseElement ReverseAt(const std::size_t & i,
-        const std::uint8_t & off, const std::uint8_t & sz) const;
+        const std::uint8_t & sz) const;
 public:
     inline std::size_t Next(const std::size_t & i, 
         const std::size_t & st = 1) const;
@@ -117,6 +122,7 @@ inline std::shared_ptr<bytes::Trait>
 }
     
 inline Segment::Segment() :
+    m_offset(0),
     m_begin(0),
     m_end(0),
     m_object(nullptr),
@@ -125,6 +131,7 @@ inline Segment::Segment() :
 
 inline Segment::Segment(const std::size_t & bg, const std::size_t & ed,
     const std::shared_ptr<bytes::ptr::Object> & o) :
+        m_offset(0),
         m_begin(bg),
         m_end(ed),
         m_object(o),
@@ -134,14 +141,37 @@ inline Segment::Segment(const std::size_t & bg, const std::size_t & ed,
 inline Segment::Segment(const std::size_t & bg, const std::size_t & ed,
     const std::shared_ptr<bytes::ptr::Object> & o,
     const std::shared_ptr<bytes::Trait> & t) :
+        m_offset(0),
         m_begin(bg),
         m_end(ed),
         m_object(o),
         m_trait(Trait(t))
 {}
 
+inline Segment::Segment(const std::size_t & bg, const std::size_t & ed,
+    const std::uint8_t & off, 
+    const std::shared_ptr<bytes::ptr::Object> & o) :
+        m_offset(off % 8),
+        m_begin(bg + (off / 8)),
+        m_end(ed + (off / 8)),
+        m_object(o),
+        m_trait(Trait())
+{}
+
+inline Segment::Segment(const std::size_t & bg, const std::size_t & ed,
+    const std::uint8_t & off, 
+    const std::shared_ptr<bytes::ptr::Object> & o,
+    const std::shared_ptr<bytes::Trait> & t) :
+        m_offset(off % 8),
+        m_begin(bg + (off / 8)),
+        m_end(ed + (off / 8)),
+        m_object(o),
+        m_trait(Trait(t))
+{}
+
 inline Segment::~Segment()
 {
+    m_offset = 0;
     m_begin = 0;
     m_end = 0;
     m_object = nullptr;
@@ -149,6 +179,7 @@ inline Segment::~Segment()
 }
 
 inline Segment::Segment(const Segment & cpy) :
+    m_offset(0),
     m_begin(0),
     m_end(cpy.Size()),
     m_object(nullptr),
@@ -162,11 +193,13 @@ inline Segment::Segment(const Segment & cpy) :
 }
 
 inline Segment::Segment(Segment && mov) :
+    m_offset(mov.m_offset),
     m_begin(mov.m_begin),
     m_end(mov.m_end),
     m_object(mov.m_object),
     m_trait(mov.m_trait)
 {
+    mov.m_offset = 0;
     mov.m_begin = 0;
     mov.m_end = 0;
     mov.m_object = nullptr;
@@ -175,6 +208,7 @@ inline Segment::Segment(Segment && mov) :
 
 inline Segment & Segment::operator=(const Segment & cpy)
 {
+    m_offset = 0;
     m_begin = 0;
     m_end = cpy.Size();
     m_object = nullptr;
@@ -189,10 +223,12 @@ inline Segment & Segment::operator=(const Segment & cpy)
 
 inline Segment & Segment::operator=(Segment && mov)
 {
+    m_offset = mov.m_offset;
     m_begin = mov.m_begin;
     m_end = mov.m_end;
     m_object = mov.m_object;
     m_trait = mov.m_trait;
+    mov.m_offset = 0;
     mov.m_begin = 0;
     mov.m_end = 0;
     mov.m_object = nullptr;
@@ -218,6 +254,17 @@ inline bool Segment::
     return b == m_object;
 }
 
+inline std::uint8_t Segment::Offset() const
+{
+    return m_offset;
+}
+
+inline void Segment::Offset(const std::uint8_t & off)
+{
+    m_offset = off % 8;
+    m_begin += off / 8;
+}
+
 inline std::size_t Segment::Begin() const
 {
     return m_begin;
@@ -230,7 +277,7 @@ inline void Segment::Begin(const std::size_t & bg)
 
 inline std::size_t Segment::End() const
 {
-    return m_end;
+    return m_end - (m_offset == 0 ? 0 : 1);
 }
 
 inline void Segment::End(const std::size_t & ed)
@@ -240,77 +287,69 @@ inline void Segment::End(const std::size_t & ed)
 
 inline std::size_t Segment::Size() const
 {
-    return (m_end > m_begin ? m_end - m_begin : 0);
+    if (m_end > m_begin)
+    {
+        const std::size_t size = m_end - m_begin;
+        if (m_offset == 0) return size;
+        return size - 1;
+    }
+    return 0;
 }
 
-inline std::size_t Segment::Resize(const std::size_t & s)
+inline std::size_t Segment::Reallocate(const std::size_t & s)
 {
     const std::size_t cs = Size();
     if (cs == s) return cs;
     const std::size_t ns = m_trait->Resize(cs, s);
-    if (cs == ns) return ns;
-    m_end = m_begin + ns;
-    return ns;
+    if (cs == ns) return cs;
+    if (m_offset == 0)
+    {
+        m_end = m_begin + ns;
+        return ns;
+    }
+    const std::size_t next_ns = ns + 1;
+    m_end = m_begin + next_ns;
+    return next_ns;
 }
 
 inline bytes::Element Segment::At(const std::size_t & i)
 {
     if (!m_object || !m_trait) return bytes::Element();
-    return bytes::Element(i, m_begin, m_end, m_object, m_trait);
+    return bytes::Element(i, m_begin, m_end, m_offset, m_object, m_trait);
 }
 
 inline bytes::Element Segment::At(const std::size_t & i, 
-    const std::uint8_t & off)
+    const std::uint8_t & sz)
 {
     if (!m_object || !m_trait) return bytes::Element();
-    return bytes::Element(i, m_begin, m_end, off, m_object, m_trait);
-}
-
-inline bytes::Element Segment::At(const std::size_t & i, 
-    const std::uint8_t & off, const std::uint8_t & sz)
-{
-    if (!m_object || !m_trait) return bytes::Element();
-    return bytes::Element(i, m_begin, m_end, off, sz, m_object, m_trait);
+    return bytes::Element(i, m_begin, m_end, m_offset, sz, m_object, m_trait);
 }
 
 inline const bytes::Element Segment::At(const std::size_t & i) const
 {
     if (!m_object || !m_trait) return bytes::Element();
-    return bytes::Element(i, m_begin, m_end, m_object, m_trait);
+    return bytes::Element(i, m_begin, m_end, m_offset, m_object, m_trait);
 }
 
 inline const bytes::Element Segment::At(const std::size_t & i, 
-    const std::uint8_t & off) const
+    const std::uint8_t & sz) const
 {
     if (!m_object || !m_trait) return bytes::Element();
-    return bytes::Element(i, m_begin, m_end, off, m_object, m_trait);
-}
-
-inline const bytes::Element Segment::At(const std::size_t & i, 
-    const std::uint8_t & off, const std::uint8_t & sz) const
-{
-    if (!m_object || !m_trait) return bytes::Element();
-    return bytes::Element(i, m_begin, m_end, off, sz, m_object, m_trait);
+    return bytes::Element(i, m_begin, m_end, m_offset, sz, m_object, m_trait);
 }
 
 inline bytes::ReverseElement Segment::ReverseAt(const std::size_t & i)
 {
     if (!m_object || !m_trait) return bytes::ReverseElement();
-    return bytes::ReverseElement(i, m_begin, m_end, m_object, m_trait);
+    return bytes::ReverseElement(i, m_begin, m_end, m_offset, m_object, 
+        m_trait);
 }
 
 inline bytes::ReverseElement Segment::ReverseAt(const std::size_t & i,
-    const std::uint8_t & off)
+    const std::uint8_t & sz)
 {
     if (!m_object || !m_trait) return bytes::ReverseElement();
-    return bytes::ReverseElement(i, m_begin, m_end, off, m_object, m_trait);
-}
-
-inline bytes::ReverseElement Segment::ReverseAt(const std::size_t & i,
-    const std::uint8_t & off, const std::uint8_t & sz)
-{
-    if (!m_object || !m_trait) return bytes::ReverseElement();
-    return bytes::ReverseElement(i, m_begin, m_end, off, sz, m_object, 
+    return bytes::ReverseElement(i, m_begin, m_end, m_offset, sz, m_object, 
         m_trait);
 }
 
@@ -322,32 +361,25 @@ inline const bytes::ReverseElement
 }
 
 inline const bytes::ReverseElement Segment::ReverseAt(const std::size_t & i,
-    const std::uint8_t & off) const
+    const std::uint8_t & sz) const
 {
     if (!m_object || !m_trait) return bytes::ReverseElement();
-    return bytes::ReverseElement(i, m_begin, m_end, off, m_object, m_trait);
-}
-
-inline const bytes::ReverseElement Segment::ReverseAt(const std::size_t & i,
-    const std::uint8_t & off, const std::uint8_t & sz) const
-{
-    if (!m_object || !m_trait) return bytes::ReverseElement();
-    return bytes::ReverseElement(i, m_begin, m_end, off, sz, m_object, 
+    return bytes::ReverseElement(i, m_begin, m_end, m_offset, sz, m_object, 
         m_trait);
 }
 
 inline std::size_t Segment::Next(const std::size_t & i, 
     const std::size_t & st) const
 {
-    if (!m_object || !m_trait) return m_end;
-    return m_trait->Next(i, st, m_begin, m_end);
+    if (!m_object || !m_trait) return End();
+    return m_trait->Next(i, st, Begin(), End());
 }
 
 inline std::size_t Segment::Previous(const std::size_t & i,
     const std::size_t & st) const
 {
     if (!m_object || !m_trait) return -1;
-    return m_trait->Previous(i, st, m_begin, m_end);
+    return m_trait->Previous(i, st, Begin(), End());
 }
 
 inline bool Segment::IsEnd(const std::size_t & i) const
@@ -376,7 +408,7 @@ inline const std::uint8_t * Segment::Get() const
 
 inline Segment::operator bool() const
 {
-    return Begin() != End() && !m_object && !*m_object;
+    return Begin() < End() && !m_object && !*m_object;
 }
 
 inline bytes::Element Segment::operator[](const std::size_t & i)
@@ -393,8 +425,12 @@ inline bool Segment::operator==(const Segment & b) const
 {
     const bool ca = static_cast<bool>(*this),
         cb = static_cast<bool>(b);
-    return (!ca && !cb) || (ca && cb && m_begin == b.m_begin &&
-        m_end == b.m_end && m_trait == b.m_trait);
+    return (!ca && !cb) || (ca && cb && 
+        IsSameObject(b.m_object) &&
+        m_offset == b.m_offset &&
+        m_begin == b.m_begin &&
+        m_end == b.m_end && 
+        m_trait == b.m_trait);
 }
 
 inline bool Segment::operator!=(const Segment & b) const
